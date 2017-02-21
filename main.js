d74cfc863653e352081a32dc654cf628d0fe17e0
Scott Heinrich <scottaheinrich@gmail.com>
2017-02-20 18:24:00 -0700

    Got scaling kind of working

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let inputStage = {};
 
+var size = 5;
+
 let imgFloor = new Image();
 imgFloor.isReady = false;
 imgFloor.onload = function() {
 	this.isReady = true;
 };
-imgFloor.src = 'floor.png';
+imgFloor.src = 'sand.jpg';
 
 function createCharacter(imageSource, location) {
 	let image = new Image();
@@ -21,74 +23,93 @@ function createCharacter(imageSource, location) {
 }
 
 let maze = [];
-for (let row = 0; row < 3; row++) {
-	maze.push([]);
-	for (let col = 0; col < 3; col++) {
-		maze[row].push({
-			x: col, y: row, edges: {
-				n: null,
-				s: null,
-				w: null,
-				e: null
-			}
-		});
+initializeMaze();
+window.onload = function(){
+	document.getElementById('sizeSelector').onclick = function(){
+		size = $("input:radio[name=size]:checked").val();
+		initializeMaze();
 	}
 }
 
-maze[0][0].edges.s = maze[1][0];
+function initializeMaze(){
+	for (let row = 0; row < size; row++) {
+		maze.push([]);
+		for (let col = 0; col < size; col++) {
+			maze[row].push({
+				x: col, y: row, edges: {
+					n: null,
+					s: null,
+					w: null,
+					e: null
+				}
+			});
+		}
+	}
+	maze[0][0].edges.s = maze[1][0];
 
-maze[0][1].edges.s = maze[1][1];
-maze[0][1].edges.e = maze[0][2];
+	maze[0][1].edges.s = maze[1][1];
+	maze[0][1].edges.e = maze[0][2];
 
-maze[0][2].edges.w = maze[0][1];
-maze[0][2].edges.s = maze[1][2];
+	maze[0][2].edges.w = maze[0][1];
+	maze[0][2].edges.s = maze[1][2];
 
-maze[1][0].edges.n = maze[0][0];
-maze[1][0].edges.e = maze[1][1];
-maze[1][0].edges.s = maze[2][0];
+	maze[1][0].edges.n = maze[0][0];
+	maze[1][0].edges.e = maze[1][1];
+	maze[1][0].edges.s = maze[2][0];
 
-maze[1][1].edges.n = maze[0][1];
-maze[1][1].edges.s = maze[2][1];
-maze[1][1].edges.w = maze[1][0];
+	maze[1][1].edges.n = maze[0][1];
+	maze[1][1].edges.s = maze[2][1];
+	maze[1][1].edges.w = maze[1][0];
 
-maze[1][2].edges.n = maze[0][2];
+	maze[1][2].edges.n = maze[0][2];
+	maze[1][2].edges.s = maze[2][2];
 
-maze[2][0].edges.n = maze[1][0];
+	maze[2][0].edges.n = maze[1][0];
 
-maze[2][1].edges.n = maze[1][1];
-maze[2][1].edges.e = maze[2][2];
+	maze[2][1].edges.n = maze[1][1];
+	maze[2][1].edges.e = maze[2][2];
 
-maze[2][2].edges.w = maze[2][1];
+	maze[2][2].edges.w = maze[2][1];
+	maze[2][2].edges.n = maze[1][2];
+
+	maze[3][3].edges.e = maze[3][4];
+	maze[3][4].edges.w = maze[3][3];
+}
+
+
+
+
+
 
 function drawCell(cell) {
 
 	if (imgFloor.isReady) {
 		context.drawImage(imgFloor,
-		cell.x * (1000 / 3), cell.y * (1000 / 3),
-		1000 / 3, 1000 / 3);
+		cell.x * (1000 / size), cell.y * (1000 / size),
+		1000 / size, 1000 / size);
 	}
 
 	if (cell.edges.n === null) {
-		context.moveTo(cell.x * (1000 / 3), cell.y * (1000 / 3));
-		context.lineTo((cell.x + 1) * (1000 / 3), cell.y * (1000 / 3));
+		context.moveTo(cell.x * (1000 / size), cell.y * (1000 / size));
+		context.lineTo((cell.x + 1) * (1000 / size), cell.y * (1000 / size));
 		context.stroke();
 	}
 
 	if (cell.edges.s === null) {
-		context.moveTo(cell.x * (1000 / 3), (cell.y + 1) * (1000 / 3));
-		context.lineTo((cell.x + 1) * (1000 / 3), (cell.y + 1) * (1000 / 3));
+		context.moveTo(cell.x * (1000 / size), (cell.y + 1) * (1000 / size));
+		context.lineTo((cell.x + 1) * (1000 / size), (cell.y + 1) * (1000 / size));
 		context.stroke();
 	}
 
 	if (cell.edges.e === null) {
-		context.moveTo((cell.x + 1) * (1000 / 3), cell.y * (1000 / 3));
-		context.lineTo((cell.x + 1) * (1000 / 3), (cell.y + 1) * (1000 / 3));
+		context.moveTo((cell.x + 1) * (1000 / size), cell.y * (1000 / size));
+		context.lineTo((cell.x + 1) * (1000 / size), (cell.y + 1) * (1000 / size));
 		context.stroke();
 	}
 
 	if (cell.edges.w === null) {
-		context.moveTo(cell.x * (1000 / 3), cell.y * (1000 / 3));
-		context.lineTo(cell.x * (1000 / 3), (cell.y + 1) * (1000 / 3));
+		context.moveTo(cell.x * (1000 / size), cell.y * (1000 / size));
+		context.lineTo(cell.x * (1000 / size), (cell.y + 1) * (1000 / size));
 		context.stroke();
 	}
 }
@@ -96,12 +117,13 @@ function drawCell(cell) {
 function renderCharacter(character) {
 	if (character.image.isReady) {
 		context.drawImage(character.image,
-		character.location.x * (1000 / 3), character.location.y * (1000 / 3));
+			character.location.x * (1000 / size), character.location.y * (1000 / size),
+	 		(1000 / size), (1000 / size)
+		);
 	}
 }
 
 function moveCharacter(keyCode, character) {
-	console.log('keyCode: ', keyCode);
 	if (keyCode === 40) {
 		if (character.location.edges.s) {
 			character.location = character.location.edges.s;
@@ -128,20 +150,13 @@ function renderMaze() {
 	context.strokeStyle = 'rgb(255, 255, 255)';
 	context.lineWidth = 6;
 
-	for (let row = 0; row < 3; row++) {
-		for (let col = 0; col < 3; col++) {
+	for (let row = 0; row < size; row++) {
+		for (let col = 0; col < size; col++) {
 			drawCell(maze[row][col]);
 		}
 	}
 
-	context.beginPath();
-	context.moveTo(0, 0);
-	context.lineTo(999, 0);
-	context.lineTo(999, 999);
-	context.lineTo(0, 999);
-	context.closePath();
-	context.strokeStyle = 'rgb(0, 0, 0)';
-	context.stroke();
+
 }
 
 function render() {
@@ -160,6 +175,9 @@ function processInput() {
 }
 
 function gameLoop() {
+	size = $("input:radio[name=size]:checked").val();
+
+
 	processInput();
 	render();
 
@@ -169,7 +187,7 @@ function gameLoop() {
 
 let canvas = null;
 let context = null;
-var myCharacter = createCharacter('character.png', maze[0][0]);
+var myCharacter = createCharacter('anubis_R.png', maze[0][0]);
 
 function initialize() {
 	canvas = document.getElementById('canvas-main');
